911fbe2201d23e149b1d13a6619ab9e398aebc76
ParnanenJ <jiro.parnanen@student.laurea.fi>
2025-09-11 14:57:12 +0300

    ws2 0.1

---
+++ TO00BL10-JavaScript-Workshop-material-main/WS02_JS_basics2/vastaukset.js
@@ -0,0 +1,22 @@
+	// Tehtävä 1
+    document.write("Exercise 1");
+    document.write(
+      `<em><strong>"If I had nine hours to chop down a tree, I'd spend the first six sharpening my ax."</strong><p>-- Abraham Lincoln</em>`
+    );
+
+	document.write("<p>")	
+
+	// Tehtävä 2
+    function repeatingText() {
+      let i = 0;
+      while (i < 50) {
+        document.write("Morjes! ");
+        i++;
+      }
+    }
+	repeatingText()
+
+	document.write("<p>")
+
+	//Tehtävä 3
+	if 
